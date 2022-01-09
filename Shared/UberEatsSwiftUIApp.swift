78d17c49b3fe17c1562ef9944312927a84367c27
OysteinThorheim <mr.dolheim@gmail.com>
2022-01-09 19:57:44 +0100

    Initial Commit

---
+++ Shared/UberEatsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberEatsSwiftUIApp.swift
+//  Shared
+//
+//  Created by Øystein Thorheim on 09/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct UberEatsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
